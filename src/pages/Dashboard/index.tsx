8bff63b4929c58ea7733918c6d9d39296d636f67
wenderf5 <wenderfabianoalves@gmail.com>
2024-09-17 16:57:36 -0300

    feat: dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,17 @@
 import style from './index.module.css';
+import icone from '../../assets/00001-1.png';
+import { useNavigate } from 'react-router-dom';
 
-export function Dashboard(){
+export function Dashboard() {
+    const navigate = useNavigate();
 
-    return(
-        <main>
-            <h1>Olá, Você esta no dashboard!</h1>
+    return (
+        <main className={style.main}>
+            <div className={style.div_center}>
+                <span className={style.txt_ttl}>Seja bem-vindo!</span>
+                <img src={icone} width={400} alt="" />
+                <button className={style.btn} onClick={()=> navigate('/sign-in')}>Deslogar</button>
+            </div>
         </main>
     )
 }
