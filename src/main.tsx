8609e34bf08fdc708d9329f1f79d0a5d59aac84f
wenderf5 <wenderfabianoalves@gmail.com>
2024-09-21 16:33:11 -0300

    feat: new feature logout

--- src/main.tsx
+++ src/main.tsx
@@ -7,5 +7,5 @@ import { router } from './routes/Router.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
+  </StrictMode>
 )
